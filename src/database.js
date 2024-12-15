87c23b3ce99398d4066db72e2d84b40359139a2d
adriellyb <adrielly0195@gmail.com>
2024-12-14 21:53:40 -0300

    uninstall dotenv

--- src/database.js
+++ src/database.js
@@ -1,18 +1,5 @@
 const sql = require('mssql');
 
-// const config = {
-//     user: process.env.DB_USER, 
-//     password: process.env.DB_PASSWORD, 
-//     server: process.env.DB_SERVER, 
-//     database: process.env.DB_NAME,
-//     authentication: {
-//         type: 'default'
-//     },
-//     options: {
-//         encrypt: true
-//     }
-// }
-
 const config = {
     user: "ada_admin", 
     password: "@z123456", 
